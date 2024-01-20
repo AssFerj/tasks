efb09f2ae213aeda88c2abce012a8eb67ed1d802
AssFerj <assisjuniorcam@gmail.com>
2024-01-20 17:48:58 -0300

    feat: create service to create task data

---
+++ back/src/services/task.services/add.task.service.ts
@@ -0,0 +1,56 @@
+import prismaClient from "../../prisma";
+
+export interface ICreateTaskProps{
+    userId: string
+    description: string, 
+    user_id: string
+}
+
+class CreateTaskService{
+    async execute(task: ICreateTaskProps){
+        if(!task.description || !task.user_id){
+            throw new Error("Missing parameters");
+        }
+
+        const user = await prismaClient.user.findFirst({
+            where:{
+                id: task.userId
+            }
+        })
+
+        if(!user){
+            throw new Error("User not found");
+        }
+    
+        const newTask = await prismaClient.task.create({
+            data:{
+                description: task.description,
+                status: false,
+                user_id: task.userId
+            },
+            select:{
+                id: true,
+                description: true,
+                status: true,
+                user_id: true
+            }
+        })
+
+        // const addTaskToUser = await prismaClient.user.update({
+        //     where:{
+        //         id: task.user_id
+        //     },
+        //     data:{
+        //         tasks:{
+        //             connect:{
+        //                 id: newTask.id
+        //             }
+        //         }
+        //     }
+        // })
+
+        return newTask;
+    }
+}
+
+export { CreateTaskService }
