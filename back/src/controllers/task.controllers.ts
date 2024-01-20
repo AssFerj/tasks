29c2820d1b7485519d3ca021828734a44ce61ba1
AssFerj <assisjuniorcam@gmail.com>
2024-01-20 18:53:09 -0300

    feat: finish create and list tasks methods

--- back/src/controllers/task.controllers.ts
+++ back/src/controllers/task.controllers.ts
@@ -1,23 +1,21 @@
 import { FastifyReply, FastifyRequest } from "fastify";
 import { CreateTaskService } from "../services/task.services/add.task.service";
+import { ListTasksService } from "../services/task.services/list.tasks.service";
 
 
 
 class CreateTaskController{
     async handle(request: FastifyRequest, reply: FastifyReply){
         const {userId} = request.params as {userId: string}
-        const {description, user_id} = request.params as {description: string, status: string, user_id: string}
+        const {description} = request.body as {description: string, status: string, user_id: string}
         if(!userId){
             return reply.status(400).send({message: "User not found"})
         }
         if(!description){
             return reply.status(400).send({message: "Missing required field: Description"})
         }
-        if(!user_id){
-            return reply.status(400).send({message: "Missing required field: User ID"})
-        }
         const taskService = new CreateTaskService()
-        const task = await taskService.execute({userId, description, user_id})
+        const task = await taskService.execute({userId, description})
         return reply.status(201).send({
             ok: true,
             message: "Task created successfully",
@@ -29,12 +27,15 @@ class CreateTaskController{
 class ListTasksController{
     async handle(request: FastifyRequest, reply: FastifyReply){
         const {userId} = request.params as {userId: string}
-        const {taskId} = request.params as {taskId: string}
-
+        if(!userId){
+            return reply.status(400).send({message: "User not found"})
+        }
+        const taskService = new ListTasksService()
+        const tasks = await taskService.execute(userId)
         return reply.status(201).send({
             ok: true,
             message: "Tasks listted successfully",
-            data: {}
+            data: tasks
         })
     }
 }
