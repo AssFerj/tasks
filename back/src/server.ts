175fc6fcc0398e372ab1d46eb9b49666efddc315
AssFerj <assisjuniorcam@gmail.com>
2024-01-20 17:45:40 -0300

    raw: include task routes

--- back/src/server.ts
+++ back/src/server.ts
@@ -1,19 +1,26 @@
 import Fastify from 'fastify'
 import cors from '@fastify/cors'
-import { routes } from './routes'
+import { userRoutes } from './routes/user.routes'
+import { taskRoutes } from './routes/task.routes'
 
 const app = Fastify({logger: true})
 
+app.setErrorHandler((error, request, reply) => {
+    reply.code(400).send({
+        message: error.message
+    })
+})
+
 const start = async () => {
-    await app.register(cors, {origin: true})
-    await app.register(routes)
+    await app.register(cors)
+    await app.register(userRoutes)
+    await app.register(taskRoutes)
     try {
         await app.listen({port: 3333})
     } catch (error) {
         app.log.error(error)
         process.exit(1)
     }
-
 }
 
-start().then(() => console.log('server running'))
+start();
