df295b31959e1139c2fb66bcef764b3d5f3e8285
AssFerj <assisjuniorcam@gmail.com>
2024-01-21 12:20:08 -0300

    feat: add update task endpoint

--- back/src/routes/task.routes.ts
+++ back/src/routes/task.routes.ts
@@ -1,5 +1,5 @@
 import { FastifyInstance, FastifyRequest, FastifyReply } from 'fastify'
-import { CreateTaskController, DeleteTaskController, ListTasksController } from '../controllers/task.controllers'
+import { CreateTaskController, DeleteTaskController, ListTasksController, UpdateTaskController } from '../controllers/task.controllers'
 
 export async function taskRoutes(fastfy: FastifyInstance) {
     fastfy.post('/user/:userId/task', (request: FastifyRequest, reply: FastifyReply) => {
@@ -11,7 +11,7 @@ export async function taskRoutes(fastfy: FastifyInstance) {
     })
 
     fastfy.put('/user/:userId/task/:taskId', (request: FastifyRequest, reply: FastifyReply) => {
-        // return new UpdateTaskController().handle(request, reply)
+        return new UpdateTaskController().handle(request, reply)
     })
 
     fastfy.delete('/user/:userId/task/:taskId', (request: FastifyRequest, reply: FastifyReply) => {
