fdb77e3e03e62490c00a2f0f2ce1f8627144e1e8
AssFerj <assisjuniorcam@gmail.com>
2024-01-22 19:29:10 -0300

    feat: add try catch to all controllers

--- back/src/controllers/user.controller.ts
+++ back/src/controllers/user.controller.ts
@@ -7,101 +7,117 @@ import { GetUserByEmailService } from "../services/user.services/get.user.by.ema
 
 class CreateUserController{
     async handle(request: FastifyRequest, reply: FastifyReply){
-        const {name, email, password} = request.body as {name: string, email: string, password: string}
-        if(!name){
-            return reply.status(400).send({message: "Missing required field: Name"})
+        try {
+            const {name, email, password} = request.body as {name: string, email: string, password: string}
+            if(!name){
+                return reply.status(400).send({message: "Missing required field: Name"})
+            }
+            if(!email){
+                return reply.status(400).send({message: "Missing required field: E-mail"})
+            }
+            if(!password){
+                return reply.status(400).send({message: "Missing required field: Password"})
+            }
+            const userAlreadyExist = new GetUserByEmailService()
+            const result = await userAlreadyExist.execute(email)
+            if(result){
+                return reply.status(400).send({message: "User already exist"})
+            }
+            const userService = new CreateUserService()
+            const user = await userService.execute({
+                name: name,
+                email: email,
+                password: password
+            })
+            return reply.status(201).send({
+                ok: true,
+                message: "User created successfully",
+                data: user
+            })
+        } catch (error) {
+            return reply.status(500).send({message: "Internal server error"})
         }
-        if(!email){
-            return reply.status(400).send({message: "Missing required field: E-mail"})
-        }
-        if(!password){
-            return reply.status(400).send({message: "Missing required field: Password"})
-        }
-        const userAlreadyExist = new GetUserByEmailService()
-        const result = await userAlreadyExist.execute(email)
-        if(result){
-            return reply.status(400).send({message: "User already exist"})
-        }
-        const userService = new CreateUserService()
-        const user = await userService.execute({
-            name: name,
-            email: email,
-            password: password
-        })
-        return reply.status(201).send({
-            ok: true,
-            message: "User created successfully",
-            data: user
-        })
     }
 }
 
 class ListUsersController{
     async handle(request: FastifyRequest, reply: FastifyReply){
-        const userService = new ListUsersService()
-        const users = await userService.execute()
-        return reply.status(201).send({
-            ok: true,
-            message: "Users listted successfully",
-            data: users.map(user => {
-                return {
-                    id: user.id,
-                    name: user.name,
-                    email: user.email,
-                    createdAt: user.created_at,
-                    updatedAt: user.updated_at
-                }
+        try {
+            const userService = new ListUsersService()
+            const users = await userService.execute()
+            return reply.status(201).send({
+                ok: true,
+                message: "Users listted successfully",
+                data: users.map(user => {
+                    return {
+                        id: user.id,
+                        name: user.name,
+                        email: user.email,
+                        createdAt: user.created_at,
+                        updatedAt: user.updated_at
+                    }
+                })
             })
-        })
+        } catch (error) {
+            return reply.status(500).send({message: "Internal server error"})
+        }
     }
 }
 
 class DeleteUserController{
     async handle(request: FastifyRequest, reply: FastifyReply){
-        const {userId} = request.params as {userId: string}
-        if(!userId){
-            return reply.status(400).send({message: "Missing required field: Id"})
-        }
-        const userService = new DeleteUserService()
-        const deletedUser = await userService.execute(userId)
-        return reply.status(201).send({
-            ok: true,
-            message: "User deleted successfully",
-            data: {
-                id: deletedUser.id,
-                name: deletedUser.name,
-                email: deletedUser.email
+        try {
+            const {userId} = request.params as {userId: string}
+            if(!userId){
+                return reply.status(400).send({message: "Missing required field: Id"})
             }
-        })
+            const userService = new DeleteUserService()
+            const deletedUser = await userService.execute(userId)
+            return reply.status(201).send({
+                ok: true,
+                message: "User deleted successfully",
+                data: {
+                    id: deletedUser.id,
+                    name: deletedUser.name,
+                    email: deletedUser.email
+                }
+            })
+        } catch (error) {
+            return reply.status(500).send({message: "Internal server error"})
+        }
     }
 }
 
 class LoginUserController{
     async handle(request: FastifyRequest, reply: FastifyReply){
-        const {email, password} = request.body as {email: string, password: string}
-        if(!email){
-            return reply.status(400).send({message: "Missing required field: E-mail"})
-        }
-        if(!password){
-            return reply.status(400).send({message: "Missing required field: Password"})
-        }
-        const userService = new LoginUserService()
-        const loggedUser = await userService.execute({
-            email: email,
-            password: password
-        })
-        if(!loggedUser){
-            return reply.status(401).send({message: "Invalid credentials"})
-        }
-        return reply.status(201).send({
-            ok: true,
-            message: "User logged successfully",
-            data: {
-                id: loggedUser.id,
-                name: loggedUser.name,
-                email: loggedUser.email
+        try {
+            const {email, password} = request.body as {email: string, password: string}
+            if(!email){
+                return reply.status(400).send({message: "Missing required field: E-mail"})
             }
-        })
+            if(!password){
+                return reply.status(400).send({message: "Missing required field: Password"})
+            }
+            const userService = new LoginUserService()
+            const loggedUser = await userService.execute({
+                email: email,
+                password: password
+            })
+            if(!loggedUser){
+                return reply.status(401).send({message: "Invalid credentials"})
+            }
+            return reply.status(201).send({
+                ok: true,
+                message: "User logged successfully",
+                data: {
+                    id: loggedUser.id,
+                    name: loggedUser.name,
+                    email: loggedUser.email
+                }
+            })
+        } catch (error) {
+            return reply.status(500).send({message: "Internal server error"})
+        }
     }
 }
 
