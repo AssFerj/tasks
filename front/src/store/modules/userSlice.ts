f532069f892265d8e692beca044222115993a904
AssFerj <assisjuniorcam@gmail.com>
2024-01-23 23:47:47 -0300

    feat: update login method

--- front/src/store/modules/userSlice.ts
+++ front/src/store/modules/userSlice.ts
@@ -1,25 +1,3 @@
-// import { PayloadAction, createSlice } from '@reduxjs/toolkit';
-// import LogedUserType from '../../types/LoggedUserType';
-
-// const initialState: LogedUserType = {
-//   email: '',
-//   password: '',
-//   remember: false
-// };
-
-// const userSlice = createSlice({
-//   name: 'logedUser',
-//   initialState,
-//   reducers: {
-//     logedUser(_, action: PayloadAction<LogedUserType>) {
-//       return action.payload;
-//     }
-//   }
-// });
-
-// export const { logedUser } = userSlice.actions;
-// export default userSlice.reducer;
-
 import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
 import LogedUserType from '../../types/LoggedUserType';
 import { login } from '../../services/api.service';
