d3456dfd60686a04af1bdb8f8f0ce1c1abd97208
AssFerj <assisjuniorcam@gmail.com>
2024-02-23 13:59:07 -0300

    feat: change conpirght to be a function

--- front/src/components/Copyright/Copyright.tsx
+++ front/src/components/Copyright/Copyright.tsx
@@ -1,16 +1,17 @@
 import { Typography, Link } from '@mui/material';
-import React from 'react';
 
-const Copyright: React.FC = () => {
+const Copyright = () => {
   return (
-    <Typography variant="body2" color="text.secondary" align="center" sx={{ mt: 5 }}>
-      {'Copyright © '}
-      <Link color="inherit" href="https://curriculum-web-j3bj.vercel.app/index.html">
-        Assis Junior
-      </Link>{' '}
-      {new Date().getFullYear()}
-      {'.'}
-    </Typography>
+    <>
+      <Typography variant="body2" color="text.secondary" align="center" sx={{ mt: 5 }}>
+        {'Copyright © '}
+        <Link color="inherit" href="https://curriculum-web-j3bj.vercel.app/index.html">
+          Assis Junior
+        </Link>{' '}
+        {new Date().getFullYear()}
+        {'.'}
+      </Typography>
+    </>
   );
 };
 
